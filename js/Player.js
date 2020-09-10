a2117348ae71c8ea7d895ed2b21b67e8343c63a3
MidhanshDev <midhanshdevthirumurugan@gmail.com>
2020-09-10 17:33:28 +0530

    player rank

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -23,6 +24,19 @@ class Player {
     database.ref(playerIndex).set({
       name:this.name,
       distance:this.distance
+    });
+  }
+
+  getCarsAtEnd(){
+    database.ref('CarsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    });
+
+  }
+
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd: rank
     });
   }
 
